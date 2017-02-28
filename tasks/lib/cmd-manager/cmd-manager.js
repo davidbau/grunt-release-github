9b812420d9dac0306195e2b18a1ecbd9e636e47a
Daniel Arteaga <dani8art.da@gmail.com>
2017-02-28 21:23:09 +0100

    Tests

--- tasks/lib/cmd-manager/cmd-manager.js
+++ tasks/lib/cmd-manager/cmd-manager.js
@@ -4,13 +4,16 @@ var childProcess = require('child_process');
 var Promise = require('bluebird');
 var logger = require('../logger/logger');
 var ErrorModel = require('../domain/error-model');
+var grunt = require('grunt');
+
 
 module.exports = {
     gitAdd: _gitAdd,
     gitCommit: _gitCommit,
     gitTag: _gitTag,
     gitPush: _gitPush,
-    gitPushTag: _gitPushTag
+    gitPushTag: _gitPushTag,
+    npmPublish: _npmPublish
 };
 
 function _gitTag(tagName, tagMessage) {
@@ -33,24 +36,28 @@ function _gitPushTag(remote, tagName) {
     return _run('git', ['push', remote, tagName]);
 }
 
+function _npmPublish() {
+    return _run('npm', ['publish']);
+}
+
 function _run(cmd, options) {
     return new Promise(function (resolve, reject) {
         var process = childProcess.spawn(cmd, options);
 
-        process.stdout.on('data', (data) => {
-            logger.info(data.toString());
-        });
+        // process.stdout.on('data', (data) => {
+        //     logger.debug(data.toString());
+        // });
 
-        process.stderr.on('data', (data) => {
-            logger.error(data.toString());
-        });
+        // process.stderr.on('data', (data) => {
+        //     logger.debug(data.toString());
+        // });
 
         process.on('exit', (code) => {
             if (!code) {
-                logger.info("Execution has been finished.");
+                grunt.log.ok(cmd + options + "executed successfully.");
                 resolve();
             } else {
-                logger.error("Error while cmd was being executed. Exited with %s", code);
+                grunt.fail.warn("Error while cmd was being executed. Exited with %s", code);
                 reject(new ErrorModel(500, 'An error occured while cmd was executing.'));
             }
         });
