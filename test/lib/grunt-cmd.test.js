431c1d71b7f314e71c80a50f2bcf1112fc756aa1
Daniel Arteaga <dani8art.da@gmail.com>
2017-04-14 20:00:10 +0200

    update grunt-cmd tests

--- test/lib/grunt-cmd.test.js
+++ test/lib/grunt-cmd.test.js
@@ -1,26 +1,22 @@
 'use strict';
 
-var GruntManager = require('../../tasks/lib/cmd-manager/grunt-cmd');
+var gruntcmd = require('../../tasks/lib/cmd-manager/grunt-cmd');
 var grunt = require('grunt');
-var gruntcmd;
 
 exports.grunt_cmd = {
 
     setUpGrunt: function (test) {
-        gruntcmd = new GruntManager(grunt);
-
         test.done();
     },
 
     run_a_task: function (test) {
-        gruntcmd.runTask('hello').then(test.done, test.done);
+        gruntcmd.runTask(grunt, 'hello').then(test.done, test.done);
     },
 
     run_several_tasks: function (test) {
-        gruntcmd.runTasks(['hello', 'hello']).then(function () {
+        gruntcmd.runTasks(grunt, ['hello', 'hello']).then(function () {
             test.done();
-        }, function (err) {
-            console.log(err);
+        }, function () {
             test.done(1);
         });
     }
